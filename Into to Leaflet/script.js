85b511b808851de317c74b1c85fd96847fa95e54
Ricardo Oliveira <oliveira.ricardo@hotmail.com>
2016-08-16 18:25:15 -0600

    first version of NACIS maps

--- Into to Leaflet/script.js
+++ Into to Leaflet/script.js
@@ -3,28 +3,24 @@ var light = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 });
 
-var dark = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
-});
-
-//Let's add a marker
-//maptime = L.marker([39.745352, -105.001942]).bindPopup('<p style="text-align: center">This is where we are! </p> North Building at Auraria Campus');
 
 //this is how we start our map, we inform where to center it and at hat zoom lvl (greater the zoom level closer it is)
 //The 'map'refers to the div that will hold the map
 var map = L.map('map', {
-    center: [39.739800, -104.911276],
-    zoom: 11,
+    center: [38.831502, -104.804521],
+    zoom: 13,
     layers: [light]
 });
 
-// $.getJSON("./data.geojson", function(data) {
-//     points = L.geoJson(data, {
-//         onEachFeature: function(feature, layer) {
-//             layer.bindPopup(feature.properties.name + '<hr>' + feature.properties.address );
-//         }
-//         }).addTo(map);
-// });
+$.getJSON("https://spreadsheets.google.com/feeds/list/1GrSO3ihNE50QCqJtUyyE1NK7xEcKWV0F7RMnR5f_ePU/o9y5h0e/public/values?alt=json", function(data) {
+    var entry = data.feed.entry;
+
+    for (i=0; i < entry.length; i++){
+        L.marker([entry[i].gsx$latitude.$t, entry[i].gsx$longitude.$t]).bindPopup("<b>"+entry[i].gsx$businessname.$t+"</b><br><br>"+entry[i].gsx$typeofvenue.$t+"<br>"+entry[i].gsx$address.$t+"<br>"+entry[i].gsx$phonenumber.$t).addTo(map);
+        console.log(entry[i])
+    }
+
+});
 
 
 
